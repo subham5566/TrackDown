985c46df68d5ca63d03cb75bd911c75dcfbb966b
subham5566 <166425980+subham5566@users.noreply.github.com>
2024-04-10 01:29:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.set("view engine", "ejs");
 
 //Modify your URL here
-var hostURL="YOUR URL";
+var hostURL="https://trackdown-t5bo.onrender.com/";
 //TOGGLE for Shorters
 var use1pt=false;
 
